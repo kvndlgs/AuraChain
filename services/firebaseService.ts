8931a23751296cfbc605ce819c4b907885fe375a
kvndlgs <poissedonn@gmail.com>
2025-10-20 14:59:32 -0400

    hey hey

--- services/firebaseService.ts
+++ services/firebaseService.ts
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { env } from "process";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAKFDnHfzgzsJsAf9icKGby7fsw5hhPCFI",
-  authDomain: "aurachain-de2b1.firebaseapp.com",
-  projectId: "aurachain-de2b1",
-  storageBucket: "aurachain-de2b1.firebasestorage.app",
-  messagingSenderId: "190142112947",
-  appId: "1:190142112947:web:e402d5f5c823067d30bcc6",
-  measurementId: "G-HPX0VTW1Z4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
